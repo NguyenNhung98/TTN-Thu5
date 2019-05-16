eb1eb77f73704dc3f0ef2ae5d277753a52801b76
nhung <chlotica290998@gmail.com>
2019-05-16 14:09:00 +0700

    user blog

--- TTNhom/Controllers/UserController.cs
+++ TTNhom/Controllers/UserController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TTNhom.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace TTNhom.Controllers
 {
     public class UserController : Controller
     {
+        PetLandModel db = new PetLandModel();
         // GET: User
         public ActionResult Index()
         {
@@ -15,7 +19,8 @@ namespace TTNhom.Controllers
         }
         public ActionResult Blog()
         {
-            return View();
+            var blog = from b in db.Blogs select b;
+            return View(blog.ToList());
         }
         public ActionResult Contact()
         {
@@ -28,6 +33,21 @@ namespace TTNhom.Controllers
         public PartialViewResult BreadcrumbsPartial()
         {
             return PartialView();
+        }
+        public ActionResult BlogInfoPartial()
+        {
+            var lstBlog = db.Blogs.Where(a => a.New == 1).ToList();
+            return PartialView(lstBlog);
+        }
+        //public PartialViewResult listBlog(/*int ? pageIndex*/)
+        //{
+        //    var item = db.Blogs.ToList();
+        //    int _pageIndex = pageIndex ?? 1;
+        //    return PartialView(item);/*.ToPagedList(_pageIndex, 2))*/
+        //}
+        public ActionResult BlogDetail()
+        {
+            return View();
         }
     }
 }
